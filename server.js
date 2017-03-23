bc81d12dcaa4cf6a664c56a76e16a09b988fc56b
Nirmal Patel <nirmal.patel@azilen.com>
2017-03-23 17:03:14 +0530

    server js commited with route authentication middleware

--- server.js
+++ server.js
@@ -7,24 +7,32 @@ let express = require("express"),
     MongoStore = require('connect-mongo')(session),
     mongoose = require("mongoose"),
     dbVal = require('./utilities/connection');
-    console.log(dbVal);
+let ensureAuthentication = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next()
+    } else {
+        res.send("error message");
+        // res.render('index', { errmsg: "Unauthorised to use ping" });
+    }
+}
+
 require("./src/config/passport")(app);
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(session({
-    secret:"mysecret",
+    secret: "mysecret",
     store: new MongoStore({ url: 'mongodb://nirmal:nirmal@localhost:27017/SMS?authSource=admin' })
 }));
 
 // app.use("/students/",require("./controllers/student.controller"));
 app.use("/auth/", require("./controllers/auth.controller"));
-app.use("/home", passport.authenticate('local'), function (req, res) {
-    console.log(res.send("home page rendered"));
-});
+// app.use("/home", ensureAuthentication, function (req, res) {
+//     console.log(res.send("home page rendered"));
+// });
 app.use("/login", function (req, res) {
-    // console.log(req);
-    res.send(req.user);
-})
+    console.log(req);
+    res.send({ "username": req.session.passport.user });
+});
 app.listen(3000, function (req, res) {
     console.log("server is running on port :: " + 3000);
 });
